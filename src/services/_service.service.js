86e6c4425b2a835b4dcbbf393a6404a5ba953302
reetam100 <reetam.chatterjee@fortmindz.in>
2024-01-11 12:56:07 +0530

    fixed the appointment service and model

--- src/services/_service.service.js
+++ src/services/_service.service.js
@@ -13,108 +13,111 @@ const createService = async ({ name, price, description, durationInMinutes, cate
     durationInMinutes,
     service_category: category,
     service_type: serviceType,
-    beautician: cur_user._id
-  })
+    beautician: cur_user._id,
+  });
   const beautician = await Beautician.findById(cur_user._id);
-  console.log("beautician: ", beautician);
+  console.log('beautician: ', beautician);
   beautician.services?.push(service._id);
   if (!beautician.service_categories.includes(category)) {
     beautician.service_categories.push(category);
   }
   await beautician.save();
   return service;
-}
+};
 
 const getServiceById = async (service_id) => {
-  const service = await Service.findById(service_id).populate('service_category').populate('service_type')
+  const service = await Service.findById(service_id).populate('service_category').populate('service_type');
+  if (!service) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Service does not exist with this ID');
+  }
   return service;
-}
+};
 
 const getAllServices = async () => {
-  const allServices = await Service.find().populate('service_category').populate('service_type')
+  const allServices = await Service.find().populate('service_category').populate('service_type');
 
   return allServices;
-}
+};
 
 const updateService = async (service_id, updateBody) => {
-  const updatedService = await getServiceById(service_id)
+  const updatedService = await getServiceById(service_id);
   if (!updatedService) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'Service not found', false)
+    throw new ApiError(httpStatus.NOT_FOUND, 'Service not found', false);
   }
-  Object.assign(updatedService, updateBody)
-  await updatedService.save()
+  Object.assign(updatedService, updateBody);
+  await updatedService.save();
   return updatedService;
-}
+};
 
 const deleteService = async (service_id, cur_user) => {
-  const beautician = await Beautician.findById(cur_user._id)
-  beautician.services = beautician.services.filter(s => s._id != service_id);
+  const beautician = await Beautician.findById(cur_user._id);
+  beautician.services = beautician.services.filter((s) => s._id != service_id);
   await beautician.save();
-  const deletedService = await Service.findByIdAndDelete(service_id)
-  console.log(beautician)
+  const deletedService = await Service.findByIdAndDelete(service_id);
+  console.log(beautician);
   return deletedService;
-}
+};
 
 const getServicesByBeauticianId = async (beautician_id) => {
   const services = await Service.find({
-    beautician: beautician_id
-  }).populate('service_category').populate('service_type').sort({ price: 'asc' })
+    beautician: beautician_id,
+  })
+    .populate('service_category')
+    .populate('service_type')
+    .sort({ price: 'asc' });
   return services;
-}
+};
 
 const filterServices = async (filters, beauticianId) => {
-
   const aggregationPipeline = [
     {
       $lookup: {
         from: 'service_types',
         localField: 'service_type',
         foreignField: '_id',
-        as: 'service_type'
-      }
+        as: 'service_type',
+      },
     },
     {
-      $unwind: "$service_type"
+      $unwind: '$service_type',
     },
     {
       $lookup: {
         from: 'service_categories',
         localField: 'service_category',
         foreignField: '_id',
-        as: 'service_category'
-      }
+        as: 'service_category',
+      },
     },
     {
-      $unwind: "$service_category"
+      $unwind: '$service_category',
     },
     {
       $sort: {
-        "price": filters.sort_price === 'desc' ? -1 : 1
-      }
+        price: filters.sort_price === 'desc' ? -1 : 1,
+      },
     },
     {
       $match: {
-        "beautician": mongoose.Types.ObjectId(beauticianId)
-      }
-    }
-  ]
-  if (filters.tags) {
-    aggregationPipeline.push(
-      {
-        $or: [
-          {
-            'service_type': { $regex: `${filters.tag}`, $options: 'i' }
-          },
-          {
-            'service_category': { $regex: `${filters.tag}`, $options: 'i' }
-          }
-        ]
+        beautician: mongoose.Types.ObjectId(beauticianId),
       },
-    )
+    },
+  ];
+  if (filters.tags) {
+    aggregationPipeline.push({
+      $or: [
+        {
+          service_type: { $regex: `${filters.tag}`, $options: 'i' },
+        },
+        {
+          service_category: { $regex: `${filters.tag}`, $options: 'i' },
+        },
+      ],
+    });
   }
   const services = await Service.aggregate(aggregationPipeline);
   return services;
-}
+};
 
 module.exports = {
   createService,
@@ -123,5 +126,5 @@ module.exports = {
   updateService,
   deleteService,
   getServicesByBeauticianId,
-  filterServices
+  filterServices,
 };
--- src/services/beautician.service.js
+++ src/services/beautician.service.js
@@ -3,7 +3,6 @@ const { Beautician } = require('../models');
 const ApiError = require('../utils/ApiError');
 const mongoose = require('mongoose');
 
-
 const createBeautician = async (beauticianBody) => {
   if (await Beautician.isEmailTaken(beauticianBody.email)) {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Email already taken');
@@ -16,8 +15,16 @@ const createBeautician = async (beauticianBody) => {
 };
 
 const getBeauticianById = async (id) => {
-  return Beautician.findById(id).populate('services').populate('service_categories').populate('products').populate('reviews');
-}
+  const beautician = await Beautician.findById(id)
+    .populate('services')
+    .populate('service_categories')
+    .populate('products')
+    .populate('reviews');
+  if (!beautician) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Beautician with this ID does not exist!');
+  }
+  return beautician;
+};
 
 const getBeauticianDetails = async (id) => {
   const aggregationPipeline = [
@@ -26,32 +33,32 @@ const getBeauticianDetails = async (id) => {
         from: 'services',
         localField: 'services',
         foreignField: '_id',
-        as: 'services'
-      }
+        as: 'services',
+      },
     },
     {
       $lookup: {
         from: 'products',
         localField: 'products',
         foreignField: '_id',
-        as: 'products'
-      }
+        as: 'products',
+      },
     },
     {
       $lookup: {
         from: 'service_categories',
         localField: 'service_categories',
         foreignField: '_id',
-        as: 'service_categories'
-      }
+        as: 'service_categories',
+      },
     },
     {
       $lookup: {
         from: 'reviews',
         localField: 'reviews',
         foreignField: '_id',
-        as: 'reviews'
-      }
+        as: 'reviews',
+      },
     },
     {
       $addFields: {
@@ -69,36 +76,38 @@ const getBeauticianDetails = async (id) => {
     },
     {
       $match: {
-        _id: mongoose.Types.ObjectId(id)
-      }
+        _id: mongoose.Types.ObjectId(id),
+      },
     },
     {
       $project: {
-        password: 0 // Exclude the password field
-      }
-    }
-  ]
-  console.log(id)
+        password: 0, // Exclude the password field
+      },
+    },
+  ];
+  console.log(id);
   const beautician = await Beautician.aggregate(aggregationPipeline);
-  await Beautician.populate(beautician, [{
-    path: 'services.service_type',
-    model: 'Service_type'
-  }, {
-    path: 'services.service_category',
-    model: 'Service_category'
-  }, {
-    path: 'reviews.user',
-    model: 'User'
-  }])
+  await Beautician.populate(beautician, [
+    {
+      path: 'services.service_type',
+      model: 'Service_type',
+    },
+    {
+      path: 'services.service_category',
+      model: 'Service_category',
+    },
+    {
+      path: 'reviews.user',
+      model: 'User',
+    },
+  ]);
   return beautician[0];
   // return Beautician.findById(id).populate('services').populate('service_categories').populate('products').populate('reviews');
 };
 
-
 const getBeauticianByEmail = async (email) => {
   return Beautician.findOne({ email });
 };
-
 
 const updateBeauticianById = async (beauticianId, updateBody) => {
   const beautician = await getBeauticianById(beauticianId);
@@ -113,7 +122,6 @@ const updateBeauticianById = async (beauticianId, updateBody) => {
   return beautician;
 };
 
-
 const deleteBeauticianById = async (beauticianId) => {
   const beautician = await getBeauticianById(beauticianId);
   if (!beautician) {
@@ -123,7 +131,6 @@ const deleteBeauticianById = async (beauticianId) => {
   return beautician;
 };
 
-
 const getAllBeauticians = async () => {
   const aggregationPipeline = [
     {
@@ -131,32 +138,32 @@ const getAllBeauticians = async () => {
         from: 'services',
         localField: 'services',
         foreignField: '_id',
-        as: 'services'
-      }
+        as: 'services',
+      },
     },
     {
       $lookup: {
         from: 'products',
         localField: 'products',
         foreignField: '_id',
-        as: 'products'
-      }
+        as: 'products',
+      },
     },
     {
       $lookup: {
         from: 'service_categories',
         localField: 'service_categories',
         foreignField: '_id',
-        as: 'service_categories'
-      }
+        as: 'service_categories',
+      },
     },
     {
       $lookup: {
         from: 'reviews',
         localField: 'reviews',
         foreignField: '_id',
-        as: 'reviews'
-      }
+        as: 'reviews',
+      },
     },
     {
       $addFields: {
@@ -174,58 +181,66 @@ const getAllBeauticians = async () => {
     },
     {
       $project: {
-        password: 0 // Exclude the password field
-      }
-    }
-  ]
+        password: 0, // Exclude the password field
+      },
+    },
+  ];
   const beauticians = await Beautician.aggregate(aggregationPipeline);
   await Beautician.populate(beauticians, {
     path: 'services.service_type',
-    model: 'Service_type'
-  })
+    model: 'Service_type',
+  });
   return beauticians;
-}
+};
 
-const filterBeauticians = async ({ search, location, date, price_range, service_type, service_category, sort_price, avgRating }) => {
+const filterBeauticians = async ({
+  search,
+  location,
+  date,
+  price_range,
+  service_type,
+  service_category,
+  sort_price,
+  avgRating,
+}) => {
   const matchConditions = [
     {
       $or: [
         {
-          'name': { $regex: `${search}`, $options: 'i' }
+          name: { $regex: `${search}`, $options: 'i' },
         },
         {
-          'services.name': { $regex: `${search}`, $options: 'i' }
+          'services.name': { $regex: `${search}`, $options: 'i' },
         },
         {
-          'service_categories.name': { $regex: `${search}`, $options: 'i' }
-        }
-      ]
+          'service_categories.name': { $regex: `${search}`, $options: 'i' },
+        },
+      ],
     },
     {
-      'address': { $regex: `${location}`, $options: 'i' }
-    }
+      address: { $regex: `${location}`, $options: 'i' },
+    },
   ];
 
   if (date) {
     const startDate = new Date(date);
     const endDate = new Date(date);
     endDate.setHours(23, 59, 59, 999);
-    matchConditions.push(
-      {
-        $and: [
-          {
-            "availability": {
-              $elemMatch: {
-                "date": {
-                  $gte: startDate,
-                  $lte: endDate
-                }
-              }
-            }
+    matchConditions.push({
+      $and: [
+        {
+          availability: {
+            $elemMatch: {
+              date: {
+                $gte: startDate,
+                $lte: endDate,
+              },
+            },
           },
-          { "availability.isAvailable": { $eq: true } },
-        ]
-      });
+        },
+        { 'availability.isAvailable': { $eq: true } },
+      ],
+    });
   }
 
   const aggregationPipeline = [
@@ -234,32 +249,32 @@ const filterBeauticians = async ({ search, location, date, price_range, service_
         from: 'services',
         localField: 'services',
         foreignField: '_id',
-        as: 'services'
-      }
+        as: 'services',
+      },
     },
     {
       $lookup: {
         from: 'products',
         localField: 'products',
         foreignField: '_id',
-        as: 'products'
-      }
+        as: 'products',
+      },
     },
     {
       $lookup: {
         from: 'service_categories',
         localField: 'service_categories',
         foreignField: '_id',
-        as: 'service_categories'
-      }
+        as: 'service_categories',
+      },
     },
     {
       $lookup: {
         from: 'reviews',
         localField: 'reviews',
         foreignField: '_id',
-        as: 'reviews'
-      }
+        as: 'reviews',
+      },
     },
     {
       $addFields: {
@@ -277,46 +292,53 @@ const filterBeauticians = async ({ search, location, date, price_range, service_
     },
     {
       $match: {
-        $or: matchConditions
-      }
+        $or: matchConditions,
+      },
     },
     {
       $project: {
-        password: 0 // Exclude the password field
-      }
-    }
+        password: 0, // Exclude the password field
+      },
+    },
   ];
-
 
   const result = await Beautician.aggregate(aggregationPipeline);
   await Beautician.populate(result, {
     path: 'services.service_type',
-    model: 'Service_type'
-  })
+    model: 'Service_type',
+  });
   let filteredResults;
-  if (price_range !== null || service_type !== null || service_category !== null || sort_price !== null || avgRating !== null) {
-    // console.log("--------------") 
+  if (
+    price_range !== null ||
+    service_type !== null ||
+    service_category !== null ||
+    sort_price !== null ||
+    avgRating !== null
+  ) {
+    // console.log("--------------")
     // console.log(avgRating)
     let ratingCondition = true;
     let priceCondition = true;
     let categoryCondition = true;
-    let typeCondition = true
-    filteredResults = result.filter(beautician => {
+    let typeCondition = true;
+    filteredResults = result.filter((beautician) => {
       if (avgRating) {
         ratingCondition = beautician.avgRating === avgRating;
         // console.log("rating: ", ratingCondition)
       }
       if (price_range) {
-        priceCondition = beautician.services.some(service => service.price >= price_range.minPrice && service.price <= price_range.maxPrice)
+        priceCondition = beautician.services.some(
+          (service) => service.price >= price_range.minPrice && service.price <= price_range.maxPrice
+        );
         // console.log(priceCondition)
       }
       if (service_category) {
-        categoryCondition = beautician.service_categories.some(service => {
+        categoryCondition = beautician.service_categories.some((service) => {
           return service.name === service_category;
         });
       }
       if (service_type) {
-        typeCondition = beautician.services.some(service => service.service_type.name === service_type);
+        typeCondition = beautician.services.some((service) => service.service_type.name === service_type);
       }
       // if(sort_price) {
       //   const sortCondition = beautician.services.some()
@@ -332,19 +354,15 @@ const filterBeauticians = async ({ search, location, date, price_range, service_
         const priceA = a.services[0].price;
         const priceB = b.services[0].price;
 
-        return sort_price === "asc" ? priceA - priceB : priceB - priceA;// Adjust the sorting logic as needed
+        return sort_price === 'asc' ? priceA - priceB : priceB - priceA; // Adjust the sorting logic as needed
       });
     }
   } else {
     filteredResults = result;
   }
 
-
-
   return filteredResults;
-}
-
-
+};
 
 module.exports = {
   createBeautician,
@@ -355,5 +373,5 @@ module.exports = {
   deleteBeauticianById,
   getAllBeauticians,
   filterBeauticians,
-  getBeauticianDetails
+  getBeauticianDetails,
 };
