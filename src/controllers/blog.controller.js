aa1a57e30ca99f869d6681008bbd4ac5675b3bb5
reetam100 <reetam.chatterjee@fortmindz.in>
2024-02-20 15:43:04 +0530

    fix: blog category by ID api and blog api

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -5,21 +5,20 @@ const catchAsync = require('../utils/catchAsync');
 const { blogService } = require('../services');
 const ApiSuccess = require('../utils/ApiSuccess');
 
-
 const createBlog = catchAsync(async (req, res) => {
-  console.log("cur_user: ", req.user);
-  const createdBlog = await blogService.createBlog(req.body, req.user)
+  console.log('cur_user: ', req.user);
+  const createdBlog = await blogService.createBlog(req.body, req.user);
   return res.status(201).json({
     code: httpStatus.CREATED,
-    message: "Blog created successfully!",
+    message: 'Blog created successfully!',
     isSuccess: true,
-    data: createdBlog
-  })
-})
+    data: createdBlog,
+  });
+});
 
 const getBlogs = catchAsync(async (req, res) => {
   let blogs = [];
-  const options = pick(req.query, ['limit', 'page'])
+  const options = pick(req.query, ['limit', 'page']);
   const page = parseInt(options.page) || 1; // Current page, default to 1 if not provided
   const limit = parseInt(options.limit) || 10;
   if (req.body.blogCategory) {
@@ -43,7 +42,7 @@ const getBlogs = catchAsync(async (req, res) => {
     blogs = await blogService.getBlogs();
   }
   if (!blogs) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'No Blogs found', false)
+    throw new ApiError(httpStatus.NOT_FOUND, 'No Blogs found', false);
   }
   const skip = (page - 1) * limit;
   const paginatedBlogs = blogs.slice(skip, skip + limit);
@@ -56,42 +55,42 @@ const getBlogs = catchAsync(async (req, res) => {
       totalPages: Math.ceil(blogs.length / limit),
       currentPage: page,
       limit: limit,
-      totalResults: paginatedBlogs.length
-    }
-  })
-})
+      totalResults: paginatedBlogs.length,
+    },
+  });
+});
 
 const getBlog = catchAsync(async (req, res) => {
-  const blog_id = req.params.blog_id;
+  const blog_id = req.query.blog_id;
   const blog = await blogService.getBlogById(blog_id);
   if (!blog) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'Blog not found', false)
+    throw new ApiError(httpStatus.NOT_FOUND, 'Blog not found', false);
   }
   return new ApiSuccess(res, httpStatus.OK, 'Blogs fetched successfully', blog);
-})
+});
 
 const updateBlog = catchAsync(async (req, res) => {
   const blog_id = req.params.blog_id;
   const blog = await blogService.updateBlog(blog_id, req.body);
   return new ApiSuccess(res, httpStatus.OK, 'Blog updated successfully', blog);
-})
+});
 
 const deleteBlog = catchAsync(async (req, res) => {
   const blog_id = req.params.blog_id;
   const blog = await blogService.deleteBlog(blog_id);
   return new ApiSuccess(res, httpStatus.NO_CONTENT, 'Blog deleted successfully', blog);
-})
+});
 
 const createBlogCategory = catchAsync(async (req, res) => {
   const { name } = req.body;
   const blog_category = await blogService.createBlogCategory(name);
   return new ApiSuccess(res, httpStatus.CREATED, 'Blog category created successfuly', blog_category);
-})
+});
 
 const getRelatedBlogs = catchAsync(async (req, res) => {
   const blogs = await blogService.getRelatedBlogs(req.params.blogId);
-  return new ApiSuccess(res, httpStatus.OK, "Related blogs", blogs)
-})
+  return new ApiSuccess(res, httpStatus.OK, 'Related blogs', blogs);
+});
 
 module.exports = {
   createBlog,
@@ -100,5 +99,5 @@ module.exports = {
   updateBlog,
   deleteBlog,
   createBlogCategory,
-  getRelatedBlogs
+  getRelatedBlogs,
 };
